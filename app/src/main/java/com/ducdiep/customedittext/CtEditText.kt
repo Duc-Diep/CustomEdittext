c38db40168a5f344fddb13c78deaf89dc7a1558e
Duc-Diep <ducdiep2k@gmail.com>
2022-02-17 09:38:21 +0700

    fix input character in between when length > max

--- app/src/main/java/com/ducdiep/customedittext/CtEditText.kt
+++ app/src/main/java/com/ducdiep/customedittext/CtEditText.kt
@@ -19,6 +19,7 @@ class CtEditText : FrameLayout {
     private var maxLength = 20
     private var isPaste = false
     private var currentFilter = maxLength
+    private var beforeText = ""
 
     constructor(context: Context) : super(context) {
         initView()
@@ -33,22 +34,23 @@ class CtEditText : FrameLayout {
 
     private fun initView() {
         inflate(context, R.layout.custom_edittext, this)
-//        edit_text.setUpdateListener(object : MyEditText.UpdateListener {
-//            override fun onCut() = Unit
-//
-//            override fun onCopy() = Unit
-//
-//            override fun onPaste() {
-//                isPaste = true
-//                Log.d("result", "onPaste: ")
-//
-//            }
-//        })
+        edit_text.setUpdateListener(object : MyEditText.UpdateListener {
+            override fun onCut() = Unit
+
+            override fun onCopy() = Unit
+
+            override fun onPaste() {
+                isPaste = true
+                Log.d("result", "onPaste: ")
+
+            }
+        })
         tv_warning.text = "$currentLength/$maxLength"
 
         edit_text.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 lastLength = s?.length!!
+                beforeText = s.toString()
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
@@ -56,25 +58,44 @@ class CtEditText : FrameLayout {
             override fun afterTextChanged(s: Editable?) {
                 Log.d("result", "afterTextChanged: ")
                 currentLength = s?.length!!
-                if (currentLength == maxLength + 1 && lastLength < currentLength) {//case lastLength < currentLength delete when length is max + 1
-                    edit_text.filters = arrayOf(InputFilter.LengthFilter(maxLength))
-                    edit_text.text = s.delete(maxLength, currentLength)
-                    edit_text.setSelection(edit_text.length())
+                if (currentLength == maxLength + 1 && lastLength < currentLength && lastLength != 0) {//case lastLength < currentLength delete when length is max + 1
+                    if (s.toString().indexOf(beforeText)!=0){
+                        edit_text.filters = arrayOf(InputFilter.LengthFilter(maxLength))
+                        edit_text.setText(beforeText)
+                        edit_text.setSelection(edit_text.length())
+                    }else{
+                        edit_text.filters = arrayOf(InputFilter.LengthFilter(maxLength))
+                        edit_text.text = s.delete(maxLength, currentLength)
+                        edit_text.setSelection(edit_text.length())
+                    }
                     tv_warning.setTextColor(Color.RED)
                     tv_warning.text = "Không được nhập quá $maxLength ký tự"
                     Log.d("length", "afterTextChanged: abcbbcbc")
                 } else if (currentLength > maxLength) {
-                    if (currentLength == currentFilter +1 && lastLength < currentLength) {
-                        edit_text.filters = arrayOf(InputFilter.LengthFilter(currentFilter))
-                        edit_text.text = s.delete(currentFilter, currentLength)
-                        edit_text.setSelection(edit_text.length())
-                    }else{
-                        edit_text.filters = arrayOf(InputFilter.LengthFilter(currentLength+1))
+                    if (currentLength == currentFilter + 1 && lastLength < currentLength) {
+                        if (s.toString().indexOf(beforeText)!=0){
+                            edit_text.filters = arrayOf(InputFilter.LengthFilter(currentFilter))
+                            edit_text.setText(beforeText)
+                            edit_text.setSelection(edit_text.length())
+                        }else{
+                            edit_text.filters = arrayOf(InputFilter.LengthFilter(currentFilter))
+                            edit_text.text = s.delete(currentFilter, currentLength)
+                            edit_text.setSelection(edit_text.length())
+                        }
+
+                        Log.d("test", "add1: ")
+
+                    } else {
+                        edit_text.filters = arrayOf(InputFilter.LengthFilter(currentLength + 1))
+                        Log.d("test", "else2: ")
                     }
                     currentFilter = currentLength
                     tv_warning.setTextColor(Color.RED)
                     tv_warning.text = "Không được nhập quá $maxLength ký tự"
-                    Log.d("length", "afterTextChanged: currentfilter: $currentFilter, s: ${s.toString()}, edit text: ${edit_text.text}")
+                    Log.d(
+                        "length",
+                        "afterTextChanged: currentfilter: $currentFilter, s: ${s.toString()}, edit text: ${edit_text.text}"
+                    )
                 } else {
                     edit_text.filters = arrayOf()
                     tv_warning.setTextColor(Color.BLUE)
